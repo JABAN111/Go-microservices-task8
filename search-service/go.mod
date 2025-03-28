a5c6ea79058884bfb13bf19a8ff28a4ffb99d062
jaba <368601@edu.itmo.ru>
2025-03-28 23:35:11 +0300

    update proto

--- search-service/go.mod
+++ search-service/go.mod
@@ -30,6 +30,7 @@ require (
 	github.com/stretchr/testify v1.10.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- search-service/proto/search/search_grpc.pb.go
+++ search-service/proto/search/search_grpc.pb.go
@@ -8,7 +8,6 @@ package update
 
 import (
 	context "context"
-
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
@@ -21,8 +20,9 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	Search_Ping_FullMethodName   = "/update.Search/Ping"
-	Search_Search_FullMethodName = "/update.Search/Search"
+	Search_Ping_FullMethodName    = "/update.Search/Ping"
+	Search_Search_FullMethodName  = "/update.Search/Search"
+	Search_ISearch_FullMethodName = "/update.Search/ISearch"
 )
 
 // SearchClient is the client API for Search service.
@@ -31,6 +31,7 @@ const (
 type SearchClient interface {
 	Ping(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error)
 	Search(ctx context.Context, in *SearchRequest, opts ...grpc.CallOption) (*RecommendedComics, error)
+	ISearch(ctx context.Context, in *SearchRequest, opts ...grpc.CallOption) (*RecommendedComics, error)
 }
 
 type searchClient struct {
@@ -61,12 +62,23 @@ func (c *searchClient) Search(ctx context.Context, in *SearchRequest, opts ...gr
 	return out, nil
 }
 
+func (c *searchClient) ISearch(ctx context.Context, in *SearchRequest, opts ...grpc.CallOption) (*RecommendedComics, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(RecommendedComics)
+	err := c.cc.Invoke(ctx, Search_ISearch_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // SearchServer is the server API for Search service.
 // All implementations must embed UnimplementedSearchServer
 // for forward compatibility.
 type SearchServer interface {
 	Ping(context.Context, *emptypb.Empty) (*emptypb.Empty, error)
 	Search(context.Context, *SearchRequest) (*RecommendedComics, error)
+	ISearch(context.Context, *SearchRequest) (*RecommendedComics, error)
 	mustEmbedUnimplementedSearchServer()
 }
 
@@ -82,6 +94,9 @@ func (UnimplementedSearchServer) Ping(context.Context, *emptypb.Empty) (*emptypb
 }
 func (UnimplementedSearchServer) Search(context.Context, *SearchRequest) (*RecommendedComics, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Search not implemented")
+}
+func (UnimplementedSearchServer) ISearch(context.Context, *SearchRequest) (*RecommendedComics, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ISearch not implemented")
 }
 func (UnimplementedSearchServer) mustEmbedUnimplementedSearchServer() {}
 func (UnimplementedSearchServer) testEmbeddedByValue()                {}
@@ -140,6 +155,24 @@ func _Search_Search_Handler(srv interface{}, ctx context.Context, dec func(inter
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Search_ISearch_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SearchRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SearchServer).ISearch(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Search_ISearch_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SearchServer).ISearch(ctx, req.(*SearchRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Search_ServiceDesc is the grpc.ServiceDesc for Search service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -154,6 +187,10 @@ var Search_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Search",
 			Handler:    _Search_Search_Handler,
+		},
+		{
+			MethodName: "ISearch",
+			Handler:    _Search_ISearch_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
